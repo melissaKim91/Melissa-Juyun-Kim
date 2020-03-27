03548378052eb91063c424e42736141be663a88a
melissaKim91 <juyunkim91@gmail.com>
2020-03-27 20:36:51 +0900

    Create 3주차크롤링.py

---
+++ 3주차크롤링.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+soup = BeautifulSoup(data.text, 'html.parser')
+#############################
+# (입맛에 맞게 코딩)
+#############################
+ms_info = soup.select('tbody td.info')
+
+for sing in ms_info:
+
+    print('-'*50)
+
+    ms_title = sing.select('a.title')
+    ms_title_el = ms_title[0].text
+    print(ms_title_el.strip())
+
+    ms_singer = sing.select('a.artist')
+    ms_singer_el = ms_singer[0].text
+    print(ms_singer_el)
+
+    ms_album = sing.select('a.albumtitle')
+    ms_album_el = ms_album[0].text
+    print(ms_album_el)
